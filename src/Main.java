214f87892be23b0fc346ab86e7b11c10f6aeb769
1the <mlolo7811@gmail.com>
2022-12-07 01:52:50 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int n= input.nextInt();
+        char x= (char)n;
+        System.out.print(x);
+    }
+}
